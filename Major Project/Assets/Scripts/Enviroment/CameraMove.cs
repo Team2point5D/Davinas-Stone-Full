8f75a346f9847bdc403561210145e755f98fd0ae
Marcus Day <110797@student.sae.edu.au>
2015-09-10 08:57:12 +1000

    Lerping of camera now works

--- Major Project/Assets/Scripts/Enviroment/CameraMove.cs
+++ Major Project/Assets/Scripts/Enviroment/CameraMove.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour
 {
+    [Header("Distance")]
+    public float distanceX;
 
-    public float distance;
+    public float distanceY;
+
+    public float distanceZ;
+
+    public float speed;
 
     Camera mainCam;
 
@@ -14,12 +20,14 @@ public class CameraMove : MonoBehaviour
 
     PlayerBehaviour playerB;
 
+    Vector3 playerPos;
+
     // Use this for initialization
     void Start()
     {
         mainCam = Camera.main;
 
-        camLocation = mainCam.transform.localPosition;
+        //camLocation = mainCam.transform.localPosition;
 
         for (int i = 0; i < cameraLocations.Length; i++)
         {
@@ -35,11 +43,20 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        camLocation = mainCam.transform.position;
+
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.localPosition;
+
+        playerPos = new Vector3(playerPos.x + distanceX, playerPos.y + distanceY, -distanceZ);
+
+
+
+
         if (playerB.doorCollided == true)
         {
             mainCam.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
 
-            //mainCam.transform.Translate(Vector3.Lerp(gameObject.transform.localPosition,new Vector3(gameObject.transform.localPosition.x,gameObject.transform.localPosition.y,gameObject.transform.localPosition.z + distance),1));
+            mainCam.transform.position = Vector3.Lerp(camLocation, playerPos, speed * Time.fixedDeltaTime);
         }
         else
         {
