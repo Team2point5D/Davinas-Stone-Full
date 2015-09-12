904a43e198924aa85ce8f65e8aceca99eda18441
Marcus Day <110797@student.sae.edu.au>
2015-09-12 11:38:15 +1000

    Started work on Xbox controller mapping

---
+++ Major Project/Assets/Scripts/Test/ControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetButtonDown("Joystick A"))
+        {
+            print("A");
+        }
+
+        if (Input.GetButtonDown("Joystick RB"))
+        {
+            print("RB");
+        }
+
+        if (Input.GetButtonDown("Joystick LB"))
+        {
+            print("LB");
+        }
+
+        if (Input.GetAxis("Joystick RT") >= 1)
+        {
+            print("RT");
+        }
+	
+	}
+}
