3446a56130448d9344ef6995cf8e0b5508da2056
Marcus Day <110797@student.sae.edu.au>
2015-09-25 14:44:32 +1000

    Added pressure pad models to level 2
    Started on pressure pad code
    
    Work on lever code

--- Major Project/Assets/Scripts/Interactables/Lever.cs
+++ Major Project/Assets/Scripts/Interactables/Lever.cs
@@ -1,32 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lever : MonoBehaviour {
+//Marcus
+public class Lever : MonoBehaviour
+{
 
     public GameObject affectedDoor;
 
     public bool isOpen;
 
-	// Use this for initialization
-	void Start () 
+
+    GameObject leverOBJ;
+
+    Quaternion leverRot;
+
+    // Use this for initialization
+    void Start()
     {
         affectedDoor.SetActive(true);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+
+        leverOBJ = GameObject.Find("Lever");
+
+        leverRot = leverOBJ.transform.rotation;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (isOpen)
         {
             affectedDoor.SetActive(false);
+
+            // To fix
+            leverOBJ.transform.rotation = new Quaternion(0, 5, 0, 0);  
         }
         else
         {
             affectedDoor.SetActive(true);
+
+            leverOBJ.transform.rotation = leverRot;
+
         }
-	
-	}
+
+    }
 
     void OnTriggerStay(Collider col)
     {
@@ -37,7 +56,7 @@ public class Lever : MonoBehaviour {
                 print("Press");
 
                 isOpen = !isOpen;
-               
+
             }
 
         }
---
+++ Major Project/Assets/Scripts/Interactables/PressurePad.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Marcus
+public class PressurePad : MonoBehaviour {
+
+    GameObject massOBJ;
+
+    //Do stuff here
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Player")
+        {
+            //Do stuff here
+        }
+    }
+}
