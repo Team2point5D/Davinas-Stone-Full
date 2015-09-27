6d705610adb79d3d18a505df948cc85d5ab9a20a
Marcus Day <110797@student.sae.edu.au>
2015-09-27 10:45:22 +1000

    Placed bullet particle effect
    Added basic main menu
    
    Made menus folder

---
+++ Major Project/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Marcus
+public class MainMenu : MonoBehaviour
+{
+
+    public void Play()
+    {
+        // Application.LoadLevel(1);
+    }
+
+    public void Quit()
+    {
+
+
+        Application.Quit();
+    }
+}
