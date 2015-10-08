48058545b51839091c4df851f93acec0eeb034cb
unknown <mday18@SYD-1233-W.sae.au>
2015-10-08 12:01:56 +1100

    Started on moving platform pressure pad

---
+++ Major Project/Assets/Scripts/Levels/Level 2/PressurePuzzle4.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePuzzle4 : MonoBehaviour
+{
+    public GameObject movingPlatform;
+
+    public float speed;
+
+    public float changeTime;
+
+    float timer;
+
+    bool canMove;
+
+    void FixedUpdate()
+    {
+        if (canMove == true)
+        {
+            movingPlatform.GetComponent<Rigidbody>().velocity = Vector3.left * Time.fixedDeltaTime * speed;
+
+            timer += Time.deltaTime;
+        }
+
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Crate")
+        {
+            canMove = true;
+        }
+        else
+        {
+            canMove = false;
+        }
+
+    }
+
+}
