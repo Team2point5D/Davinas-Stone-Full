27c8818d06bed1df8618d3108b3c62ba72e68d68
Marcus <Day>
2015-10-29 10:02:32 +1100

    Started on new scripts
    Put shaders in folder

---
+++ Major Project/Assets/Scripts/Levels/ChangeLevels.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Marcus
+public class ChangeLevels : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Major Project/Assets/Scripts/Levels/Level 1/WindAnimate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Marcus
+public class WindAnimate : MonoBehaviour {
+
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	
+	}
+}
