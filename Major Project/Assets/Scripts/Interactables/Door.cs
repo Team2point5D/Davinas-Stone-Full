f08ff1cd0be498fa3df7ae19d7fe3ec74b021274
David Hanna <onnfire@hotmail.com>
2015-11-03 21:59:28 +1100

    Added prefabs for door entering and exiting for ease of use Prevents player from going backwards (for now at least)

---
+++ Major Project/Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+}
