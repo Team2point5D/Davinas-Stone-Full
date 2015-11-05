74b44af5a32107d181fd2413253634f285764b70
Marcus <Day>
2015-11-05 16:57:19 +1100

    Put in light script for sonar light

---
+++ Major Project/Assets/Scripts/Player/SonarLight.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Marcus
+public class SonarLight : MonoBehaviour
+{
+    [Header("Light")]
+    //public float lightMinIntensity;
+
+    public float lightMaxIntensity;
+
+    [Range(0,2)]
+    public float lightSpeed;
+
+
+    [Header("Range")]
+    //public float minRange;
+    public float maxRange;
+
+    [Range(0, 2)]
+    public float rangeSpeed;
+
+    [Space(10)]
+    public bool startLight;
+
+
+    float timer;
+
+    Light lightOBJ;
+
+    void Start()
+    {
+        lightOBJ = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        //lightSpeed = timer;
+
+        if (startLight)
+        {
+            lightOBJ.range = Mathf.Lerp(lightOBJ.range, maxRange, rangeSpeed * Time.deltaTime);
+
+            lightOBJ.intensity = Mathf.Lerp(lightOBJ.intensity, lightMaxIntensity, lightSpeed * Time.deltaTime);
+        }
+
+
+    }
+
+
+
+}
