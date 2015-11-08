29da3f4eb038c9014170c362994b5d90b6c5aa0b
Marcus Day <110797@student.sae.edu.au>
2015-11-08 12:03:42 +1100

    Added detailed main menu scene
    Made save script

---
+++ Major Project/Assets/Scripts/Game/SaveGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Marcus
+public class SaveGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
