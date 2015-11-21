066a38a53be81f65db342a163d2292d525ff81e5
Marcus Day <110797@student.sae.edu.au>
2015-11-22 10:21:05 +1100

    Started work on dialogue lines playing in levels

---
+++ Major Project/Assets/Scripts/Audio/CutSceneDialogue.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Marcus
+public class CutSceneDialogue : MonoBehaviour
+{
+
+    public AudioClip[] dialogueLines;
+
+    [Space(10)]
+
+    [Range(0, 2)]
+    public float waitTime;
+
+    [Space(5)]
+
+    public bool sceneCompleted;
+
+    public bool startScene;
+
+    float timer;
+
+    AudioSource aSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // print(timer.ToString());
+
+        if (startScene == true)
+        {
+
+            //print(i);
+
+          
+
+            aSource.PlayOneShot(dialogueLines[0]);
+
+            startScene = false;
+
+            if (!aSource.isPlaying)
+            {
+                timer += Time.deltaTime;
+            }
+
+
+            //if (timer >= waitTime)
+            //{
+            //    aSource.clip = dialogueLines[1];
+
+            //    aSource.Play();
+
+            //    startScene = false;
+
+            //    sceneCompleted = true;
+
+
+            //}
+
+
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!sceneCompleted)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                startScene = true;
+
+            }
+        }
+    }
+
+
+
+}
---
+++ Major Project/Assets/Scripts/Audio/PlayDialogue.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Marcus
+public class PlayDialogue : MonoBehaviour {
+
+    public AudioClip Dialogue;
+
+    AudioSource aSource;
+
+	// Use this for initialization
+	void Start () 
+    {
+        aSource = GetComponent<AudioSource>();
+
+      
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            aSource.clip = Dialogue;
+
+            aSource.Play();
+
+        }
+
+    }
+}
