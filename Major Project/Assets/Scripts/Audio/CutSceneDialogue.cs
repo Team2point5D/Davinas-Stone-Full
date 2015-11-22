5aa7db27bf835bcb1cdfa24f6e1047c11a4d1ce6
Marcus Day <110797@student.sae.edu.au>
2015-11-23 08:52:50 +1100

    More work on cutscene dialogue script

--- Major Project/Assets/Scripts/Audio/CutSceneDialogue.cs
+++ Major Project/Assets/Scripts/Audio/CutSceneDialogue.cs
@@ -23,56 +23,72 @@ public class CutSceneDialogue : MonoBehaviour
     AudioSource aSource;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         aSource = GetComponent<AudioSource>();
-
-
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        // print(timer.ToString());
-
         if (startScene == true)
         {
 
-            //print(i);
-
-          
-
-            aSource.PlayOneShot(dialogueLines[0]);
-
-            startScene = false;
 
             if (!aSource.isPlaying)
             {
                 timer += Time.deltaTime;
+
+
+                if (!aSource.isPlaying)
+                {
+                    StartCoroutine("StartDialogue");
+                }
             }
 
-
-            //if (timer >= waitTime)
-            //{
-            //    aSource.clip = dialogueLines[1];
-
-            //    aSource.Play();
-
-            //    startScene = false;
-
-            //    sceneCompleted = true;
-
-
-            //}
 
 
         }
 
+        //print(timer.ToString());
 
+    }
+
+
+
+    IEnumerator StartDialogue()
+    {
+        print("Start");
+
+        yield return StartCoroutine("Wait");
+
+        aSource.clip = dialogueLines[1];
+
+        aSource.Play();
+
+
+        //yield return StartCoroutine("Wait");
+
+        //aSource.clip = dialogueLines[2];
+
+        //aSource.Play();
+
+
+        //yield return StartCoroutine("Wait");
+
+        //aSource.clip = dialogueLines[3];
+
+        //aSource.Play();
+
+    }
+
+    IEnumerator Wait()
+    {
+
+        yield return new WaitForSeconds(waitTime);
+
+        print("Wait done");
     }
 
     void OnTriggerEnter(Collider col)
@@ -82,6 +98,17 @@ public class CutSceneDialogue : MonoBehaviour
             if (col.gameObject.tag == "Player")
             {
                 startScene = true;
+
+                if (startScene)
+                {
+
+                    aSource.clip = dialogueLines[0];
+
+                    aSource.Play();
+
+
+
+                }
 
             }
         }
