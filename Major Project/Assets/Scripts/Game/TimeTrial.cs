7c272fb01bcc1c389027fd76e656c5748031ac39
Marcus Day <110797@student.sae.edu.au>
2015-11-25 17:42:26 +1100

    Started work on time trial desirable script

---
+++ Major Project/Assets/Scripts/Game/TimeTrial.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeTrial : MonoBehaviour
+{
+
+    public float currentTime;
+
+    public float goalTime;
+
+    float timer;
+
+   // public Text timerText;
+
+    public bool timerRun = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string seconds = Mathf.Floor(currentTime % 60).ToString("00");
+
+        string minutes = Mathf.Floor(currentTime / 60).ToString("00");
+
+        if (timerRun == true)
+        {
+            currentTime += Time.deltaTime;
+
+           // timerText.text = "" + minutes + ":" + seconds;
+
+            print("" + minutes + ":" + seconds);
+
+            if (currentTime >= goalTime)
+            {
+                //timerRun = false;
+
+                print("Done");
+
+            }
+        }
+
+    }
+}
+
