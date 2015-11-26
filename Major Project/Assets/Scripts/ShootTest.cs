a05c041d35e069053c9b1fe1b91522b476794f82
unknown <dhanna1@SYD-1223-W.sae.au>
2015-11-26 15:20:06 +1100

    Fixes and stuff

---
+++ Major Project/Assets/Scripts/ShootTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootTest : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((Input.GetMouseButtonDown(0) || Input.GetAxis("RT") == 1))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            Debug.Log(mousePos);
+
+            GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Cube.transform.position = mousePos;
+        }
+	}
+}
